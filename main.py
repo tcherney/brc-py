095e116841b353268344900a2acd52ecb7825af9
Timothy Cherney <tjcherney@hotmail.com>
2024-08-25 00:00:15 -0700

    changed mmap/multiprocessing version to use pool because sending large objects is too much to handle, also improved polars version by changing streaming size

--- main.py
+++ main.py
@@ -1,78 +1,73 @@
 from typing import LiteralString
-import numpy as np
 import sys
+import os
 import mmap
 import time
-from multiprocessing import Process, Lock, Manager
+import multiprocessing
 
-
-
-def process(split, l, calculations):
-    #print(len(split))
-    indx = 0
+def process(file_name, start, end):
+    offset = (start // mmap.ALLOCATIONGRANULARITY) * mmap.ALLOCATIONGRANULARITY
     split_map = {}
-    while indx < len(split):
-        semi = find_sep(split, indx)
-        city_name = split[indx:semi].tobytes().decode()
-        crlf = find_sep(split, semi+1, b'\r')
-        value = float(split[semi+1:crlf].tobytes())
-        indx = crlf + 2
-        if city_name not in split_map:
-            split_map[city_name] = np.array(value)
-        else:
-            split_map[city_name] = np.append(split_map[city_name], value)
-    l.acquire()
-    #print(f"built internal map {split_map}")
-    for key, val in split_map.items():
-        if key in calculations:
-            current = calculations[key]
-            calculations[key][0] = min(current[0], np.min(val))
-            calculations[key][1] += np.sum(val)
-            calculations[key][2] = max(current[2], np.max(val))
-            calculations[key][3] += float(val.size)
-            #print(f"updating key {calculations[key]}")
-        else:    
-            calculations[key] = np.array([np.min(val), np.sum(val), np.max(val), float(val.size)])
-            #print(f"adding key {calculations[key]}")
-    l.release()
-
-def find_sep(mm, start:int =0, sep: bytes=b';'):
-    while (start < len(mm)):
-        if mm[start] == sep:
-            return start
-        else:
-            start += 1
-    return len(mm)
+    with open(file_name, "rb") as f:
+        with mmap.mmap(f.fileno(), end-offset, access=mmap.ACCESS_READ, offset=offset) as mm:
+            mm.seek(start-offset)
+            for line in iter(mm.readline, b""):
+                sep_indx = line.find(b';')
+                city_name = line[:sep_indx]
+                value = float(line[sep_indx+1:-2])
+                if city_name in split_map:
+                    current = split_map[city_name]
+                    current[0] = min(current[0], value)
+                    current[1] += value
+                    current[2] = max(current[2], value)
+                    current[3] += 1
+                else:
+                    split_map[city_name] = [value, value, value, 1]
+    #print("finished calc")
+    #print(split_map)
+    return split_map
 
 def main():
     start_time = time.time()
     file_name: LiteralString = f"measurements.txt"
     if len(sys.argv) > 1:
         file_name = sys.argv[1]
-    with open(file_name, "r", encoding="utf-8") as f:
-        mm: np.memmap = np.memmap(file_name, mode="r", dtype='S1')
-        lock = Lock()
-        NUM_PROCESS = 15
-        processes = []
+    length = os.path.getsize(file_name)
+    with open(file_name, "rb") as f:
+        mm = mmap.mmap(f.fileno(), length=0, access=mmap.ACCESS_READ)
+        NUM_PROCESS = os.cpu_count() - 1
+        splits = []
         start = 0
-        data_split = len(mm)//NUM_PROCESS
-        calculations = Manager().dict()
+        data_split = length//NUM_PROCESS
         for i in range(NUM_PROCESS):
             start_end = (i + 1) * data_split
-            end = find_sep(mm, start_end, b'\n')
-            #print(f"adding process with {start},{end}")
-            processes.append(Process(target=process, args=(mm[start:end],lock, calculations)))
-            processes[i].start()
+            end = mm.find(b'\n', start_end)
+            end = end + 1 if end != -1 else length
+            # objects too big for pickling to pass around have to work with file from each process
+            splits.append((file_name, start, end))
             start = end + 1
-            if (start >= len(mm)):
+            if (start >= length):
                 break
-        for p in processes:
-            p.join()
-            
-
+        mm.close()
+        with multiprocessing.Pool(processes=NUM_PROCESS) as pool:
+            ret = pool.starmap(process, splits)
+        
+        calculations = {}
+        for split_map in ret:
+            for key, val in split_map.items():
+                if key in calculations:
+                    current = calculations[key]
+                    calculations[key][0] = min(current[0], val[0])
+                    calculations[key][1] += val[1]
+                    calculations[key][2] = max(current[2], val[2])
+                    calculations[key][3] += val[3]
+                    #print(f"updating key {calculations[key]}")
+                else:    
+                    calculations[key] = val
+                    #print(f"adding key {calculations[key]}")
         sys.stdout.write("{")
         for key, val in calculations.items():
-            sys.stdout.write(f"{key}={val[0]:.2f}/{val[1]/val[3]:.2f}/{val[2]:.2f},")
+            sys.stdout.write(f"{key.decode()}={val[0]:.2f}/{val[1]/val[3]:.2f}/{val[2]:.2f},")
         sys.stdout.write("}\n")
 
     end_time = time.time()
--- main_polars.py
+++ main_polars.py
@@ -8,8 +8,9 @@ def main():
     file_name = f"measurements.txt"
     if len(sys.argv) > 1:
         file_name = sys.argv[1]
-    q = pl.scan_csv(file_name, separator=";", schema={"City": pl.String, "Measurement": pl.Float64}, has_header=False).group_by("City").agg([pl.min("Measurement").name.suffix("_min"),pl.mean("Measurement").name.suffix("_mean"),  pl.max("Measurement").name.suffix("_max")]).collect(streaming=True)
-    print(q)
+    pl.Config.set_streaming_chunk_size(8_000_000)
+    q = pl.scan_csv(file_name, separator=";", schema={"City": pl.String, "Measurement": pl.Float64}, has_header=False).group_by("City").agg([pl.min("Measurement").alias("min"),pl.mean("Measurement").alias("mean"),  pl.max("Measurement").alias("max")]).sort("City").collect(streaming=True)
+    print("{",", ".join(f"{data[0]}={data[1]:.2f}/{data[2]:.2f}/{data[3]:.2f}" for data in q.iter_rows()), "}", sep="")
     end = time.time()
     print(f"{end-start}s Total execution time")
     
